27dd45261765bb2f00b9193be6c0cbbb330b6705
virakaryta <virakaryta14@gmail.com>
2024-12-26 14:19:23 -0500

    added web elements locators to VehicleCost page

--- src/test/java/com/transmuda/pages/VehicleCost.java
+++ src/test/java/com/transmuda/pages/VehicleCost.java
@@ -1,6 +1,8 @@
 package com.transmuda.pages;
 
 import com.transmuda.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class VehicleCost {
@@ -8,5 +10,20 @@ public class VehicleCost {
     public VehicleCost() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+
+    @FindBy(xpath = "(//span[@class='grid-header-cell__label'])[1]")
+    public WebElement type;
+
+    @FindBy(xpath = "(//span[@class='grid-header-cell__label'])[2]")
+    public WebElement totalPrice;
+
+    @FindBy(xpath = "(//span[@class='grid-header-cell__label'])[3]")
+    public WebElement date;
+
+    @FindBy(xpath = "(//input[@type='checkbox'])[4]")
+    public WebElement checkBoxSelectAll;
+
+
 
 }
